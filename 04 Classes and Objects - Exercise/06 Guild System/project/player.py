eef5dd7eb7ac2d6f4922c1fdd81b3a4ee1978278
Petar Bekriev <petar.bekriev@gmail.com>
2023-02-27 23:39:07 +0200

    Create player.py

---
+++ 04 Classes and Objects - Exercise/06 Guild System/project/player.py
@@ -0,0 +1,27 @@
+class Player:
+
+    NOT_IN_OTHER_GUILD_WORD = "Unaffiliated"
+
+    def __init__(self, name, hp, mp):
+        self.name = name
+        self.hp = hp
+        self.mp = mp
+        self.guild = Player.NOT_IN_OTHER_GUILD_WORD
+        self.skills = {}
+
+    def add_skill(self, skill_name, mana_cost):
+        if skill_name in self.skills:
+            return f"Skill already added"
+
+        self.skills[skill_name] = mana_cost
+
+        return f"Skill {skill_name} added to the collection of the player {self.name}"
+
+    def player_info(self):
+        skills = '\n'.join(f"==={s} - {m}" for s, m in self.skills.items())
+
+        return f"Name: {self.name}\n"\
+               f"Guild: {self.guild}\n"\
+               f"HP: {self.hp}\n" \
+               f"MP: {self.mp}\n" \
+               f"{skills}"
